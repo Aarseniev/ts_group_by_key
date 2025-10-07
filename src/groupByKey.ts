e480647a924358ee6d7c62ef0b8ba25aa2994940
Artem Arseniev <144947394+Aarseniev@users.noreply.github.com>
2025-10-08 01:40:06 +0300

    fix issues

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -4,12 +4,6 @@ type GroupsMap<T> = {
 
 export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   return items.reduce<GroupsMap<T>>((acc, item) => {
-    const value = item[key];
-
-    if (value === undefined || value === null) {
-      return acc;
-    }
-
     const group = String(item[key]);
 
     if (!acc[group]) {
